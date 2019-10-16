9a67809b88f1cb79ec063dc41188f1dfba860796
Jakub Rożek <jakub@stoplight.io>
2019-10-16 19:22:57 +0200

    feat: use DeepPartial

--- src/oas3/accessors.ts
+++ src/oas3/accessors.ts
@@ -1,11 +1,12 @@
+import { DeepPartial } from '@stoplight/types';
 import { get, isObject } from 'lodash';
 import { OpenAPIObject, OperationObject, SecuritySchemeObject } from 'openapi3-ts';
 
 import { mapToKeys } from '../utils';
 
 export function getSecurities(
-  spec: Partial<OpenAPIObject>,
-  operation: Partial<OperationObject>,
+  spec: DeepPartial<OpenAPIObject>,
+  operation: DeepPartial<OperationObject>,
 ): SecuritySchemeObject[][] {
   const opSchemesPairs = operation.security ? mapToKeys(operation.security) : mapToKeys(spec.security);
   const definitions = get(spec, 'components.securitySchemes');
