8f37a70fb7ff546df0691f8f54263ffe09cd6591
Jakub Rożek <jakub@stoplight.io>
2019-12-16 09:46:17 +0100

    fix(oas2): handle invalid server host (#46)
    * fix(oas2): handle invalid server host
    
    * Update src/oas2/transformers/servers.ts

--- src/oas2/transformers/__tests__/servers.test.ts
+++ src/oas2/transformers/__tests__/servers.test.ts
@@ -1,19 +1,20 @@
 import { translateToServers } from '../servers';
 
+type GlobalWithLocation = typeof global & { location: Partial<Location> & { href: string } };
+
 describe('translateToServers', () => {
+  afterAll(() => {
+    delete (global as GlobalWithLocation).location;
+  });
+
   // Assures: https://stoplightio.atlassian.net/browse/SL-976
   test('given executed in a browser context and a query in location.href should not inherit that query', () => {
-    // @ts-ignore - ignore the fact that `location` does not exist on global object. Mimic browser.
-    const loc = global.location;
-    // @ts-ignore - ignore the fact that `location` does not exist on global object. Mimic browser.
-    global.location = {
+    (global as GlobalWithLocation).location = {
       href: 'https://www.someotherdomain.com?query=123',
     };
     expect(translateToServers({ host: 'stoplight.io' }, { schemes: ['http'] })).toEqual([
       { url: 'http://stoplight.io' },
     ]);
-    // @ts-ignore - ignore the fact that `location` does not exist on global object. Mimic browser.
-    global.location = loc;
   });
 
   test('given operation schemes should return servers', () => {
@@ -71,6 +72,10 @@ describe('translateToServers', () => {
     ]);
   });
 
+  test('given empty host should return empty array', () => {
+    expect(translateToServers({ host: '', basePath: '/base-path' }, { schemes: ['http', 'https'] })).toEqual([]);
+  });
+
   test('should handle malformed spec scheme gracefully', () => {
     expect(translateToServers({ host: 'stoplight.io', basePath: '/base-path', schemes: 1 } as any, {})).toEqual([]);
     // covers TypeError: {value}.replace is not a function coming from URI.js
@@ -91,6 +96,23 @@ describe('translateToServers', () => {
     ).toEqual([
       {
         url: 'test://stoplight.io/base-path',
+      },
+    ]);
+  });
+
+  test('should handle invalid server host gracefully', () => {
+    expect(translateToServers({ host: 123 as any, basePath: '/base-path' }, { schemes: ['http', 'https'] })).toEqual(
+      [],
+    );
+  });
+
+  test('should handle invalid server basePath gracefully', () => {
+    expect(translateToServers({ host: 'stoplight.io', basePath: 123 as any }, { schemes: ['http', 'https'] })).toEqual([
+      {
+        url: 'http://stoplight.io',
+      },
+      {
+        url: 'https://stoplight.io',
       },
     ]);
   });
