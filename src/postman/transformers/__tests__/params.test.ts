10e5eb9e2800b2f072ea8241eb2cc9a21c864dd0
Karol Maciaszek <karol@maciaszek.pl>
2020-02-14 13:40:00 +0100

    refactor: found better home for params

---
+++ src/postman/transformers/__tests__/params.test.ts
@@ -0,0 +1,361 @@
+import { INodeExample } from '@stoplight/types';
+import { Header, QueryParam, RequestBody } from 'postman-collection';
+import { transformBody, transformHeader, transformPathParams, transformQueryParam } from '../params';
+
+describe('transformQueryParam()', () => {
+  describe('value is set', () => {
+    it('transforms correctly', () => {
+      expect(transformQueryParam(new QueryParam({ key: 'testKey', value: 'testValue' }))).toEqual({
+        examples: [
+          {
+            key: 'default',
+            value: 'testValue',
+          },
+        ],
+        schema: {
+          type: 'string',
+        },
+        name: 'testKey',
+        style: 'form',
+      });
+    });
+  });
+
+  describe('value is null', () => {
+    it('transforms correctly', () => {
+      expect(transformQueryParam(new QueryParam({ key: 'testKey', value: null }))).toEqual({
+        name: 'testKey',
+        style: 'form',
+      });
+    });
+  });
+
+  describe('key is null', () => {
+    it('transforms correctly with key being empty string', () => {
+      expect(transformQueryParam(new QueryParam({ key: null, value: null }))).toEqual({
+        name: '',
+        style: 'form',
+      });
+    });
+  });
+});
+
+describe('transformHeader()', () => {
+  describe('value is defined', () => {
+    it('result contains schema', () => {
+      expect(transformHeader(new Header({ key: 'testKey', value: 'some string' }))).toEqual({
+        name: 'testkey',
+        schema: {
+          type: 'string',
+        },
+        style: 'simple',
+        examples: [
+          {
+            key: 'default',
+            value: 'some string',
+          },
+        ],
+      });
+    });
+  });
+
+  describe('value is not defined', () => {
+    it('results does not contain schema', () => {
+      expect(transformHeader(new Header({ key: 'testKey' }))).toEqual({
+        name: 'testkey',
+        style: 'simple',
+      });
+    });
+  });
+});
+
+describe('transformPathParams()', () => {
+  it('transforms correctly', () => {
+    expect(transformPathParams(['elem1', ':param1', ':param2', 'elem2'])).toEqual([
+      { name: 'param1', style: 'simple' },
+      { name: 'param2', style: 'simple' },
+    ]);
+  });
+});
+
+describe('transformBody()', () => {
+  describe('body is passed in raw mode', () => {
+    describe('body is defined', () => {
+      describe('mediaType is JSON-ish', () => {
+        describe('body is correctly defined json', () => {
+          it('returns body containing example, schema and media type', () => {
+            expect(transformBody(new RequestBody({ mode: 'raw', raw: '{"a":"b"}' }), 'application/nice+json')).toEqual({
+              contents: [
+                {
+                  examples: [{ key: 'default', value: { a: 'b' } }],
+                  mediaType: 'application/nice+json',
+                  schema: {
+                    properties: { a: { type: 'string' } },
+                    type: 'object',
+                  },
+                },
+              ],
+            });
+          });
+        });
+
+        describe('body is not a correct JSON', () => {
+          it('returns body containing example and media type', () => {
+            expect(transformBody(new RequestBody({ mode: 'raw', raw: '"a":"b"' }), 'application/json')).toEqual({
+              contents: [
+                {
+                  examples: [{ key: 'default', value: '"a":"b"' }],
+                  mediaType: 'application/json',
+                },
+              ],
+            });
+          });
+        });
+      });
+
+      describe('mediaType is not JSON-ish', () => {
+        describe('media type is defined', () => {
+          it('returns body containing example and given media type', () => {
+            expect(transformBody(new RequestBody({ mode: 'raw', raw: '<a />' }), 'application/xml')).toEqual({
+              contents: [
+                {
+                  examples: [{ key: 'default', value: '<a />' }],
+                  mediaType: 'application/xml',
+                },
+              ],
+            });
+          });
+        });
+
+        describe('media type is not defined', () => {
+          it('returns body containing example and text/plain media type', () => {
+            expect(transformBody(new RequestBody({ mode: 'raw', raw: "I'm a goat. Bleeet!" }))).toEqual({
+              contents: [
+                {
+                  examples: [{ key: 'default', value: "I'm a goat. Bleeet!" }],
+                  mediaType: 'text/plain',
+                },
+              ],
+            });
+          });
+        });
+      });
+    });
+
+    describe('body is not defined', () => {
+      it('returns no body', () => {
+        expect(transformBody(new RequestBody({ mode: 'raw' }))).toBeUndefined();
+      });
+    });
+  });
+
+  describe('body is passed in formdata mode', () => {
+    describe('body is defined', () => {
+      describe('media type is defined', () => {
+        it('returns body containing schema, example and given media type', () => {
+          expect(
+            transformBody(
+              new RequestBody({
+                mode: 'formdata',
+                formdata: [
+                  { key: 'k1', value: 'v1' },
+                  { key: 'k2', value: 'v2', description: 'd2' },
+                ],
+              }),
+              'multipart/test+form-data',
+            ),
+          ).toEqual({
+            contents: [
+              {
+                examples: [{ key: 'default', value: { k1: 'v1', k2: 'v2' } }],
+                mediaType: 'multipart/test+form-data',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    k1: { type: 'string', description: undefined },
+                    k2: { type: 'string', description: 'd2' },
+                  },
+                },
+              },
+            ],
+          });
+        });
+      });
+
+      describe('media type is not defined', () => {
+        it('returns body containing schema, example and multipart/form-data media type', () => {
+          expect(
+            transformBody(
+              new RequestBody({
+                mode: 'formdata',
+                formdata: [{ key: 'k1', value: 'v1' }],
+              }),
+            ),
+          ).toEqual({
+            contents: [
+              {
+                examples: [{ key: 'default', value: { k1: 'v1' } }],
+                mediaType: 'multipart/form-data',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    k1: { type: 'string', description: undefined },
+                  },
+                },
+              },
+            ],
+          });
+        });
+      });
+    });
+
+    describe('some keys are not defined', () => {
+      it('returns body containing schema and example with generated keys', () => {
+        const result = transformBody(
+          new RequestBody({
+            mode: 'formdata',
+            formdata: [{ value: 'v1' }, { value: 'v2', description: 'd2' }],
+          }),
+          'multipart/test+form-data',
+        );
+
+        expect(result).toEqual({
+          contents: [
+            {
+              examples: [{ key: 'default', value: expect.any(Object) }],
+              mediaType: 'multipart/test+form-data',
+              schema: {
+                type: 'object',
+                properties: expect.any(Object),
+              },
+            },
+          ],
+        });
+
+        // verify shape of schema object
+        expect(Object.entries(result!.contents![0].schema!.properties!)).toEqual(
+          expect.arrayContaining([
+            [expect.stringMatching(/^_gen_[0-9a-f]{6}$/), { type: 'string', description: undefined }],
+            [expect.stringMatching(/^_gen_[0-9a-f]{6}$/), { type: 'string', description: 'd2' }],
+          ]),
+        );
+
+        // verify shape of example object
+        expect(Object.entries((result!.contents![0].examples![0] as INodeExample).value)).toEqual(
+          expect.arrayContaining([
+            [expect.stringMatching(/^_gen_[0-9a-f]{6}$/), 'v1'],
+            [expect.stringMatching(/^_gen_[0-9a-f]{6}$/), 'v2'],
+          ]),
+        );
+
+        // ensure both share exactly the same keys
+        expect(Object.keys(result!.contents![0].schema!.properties!).sort()).toEqual(
+          Object.keys((result!.contents![0].examples![0] as INodeExample).value).sort(),
+        );
+      });
+    });
+
+    describe('body is not defined', () => {
+      it('returns default empty body', () => {
+        expect(transformBody(new RequestBody({ mode: 'formdata' }))).toEqual({
+          contents: [
+            {
+              examples: [{ key: 'default', value: {} }],
+              mediaType: 'multipart/form-data',
+              schema: { properties: {}, type: 'object' },
+            },
+          ],
+        });
+      });
+
+      it('returns no body', () => {
+        expect(transformBody({ mode: 'formdata' } as RequestBody)).toBeUndefined();
+      });
+    });
+  });
+
+  describe('body is passed in urlencoded mode', () => {
+    describe('body is defined', () => {
+      describe('media type is defined', () => {
+        it('returns body containing schema, example and given media type', () => {
+          expect(
+            transformBody(
+              new RequestBody({
+                mode: 'urlencoded',
+                urlencoded: [
+                  { key: 'k1', value: 'v1' },
+                  { key: 'k2', value: 'v2', description: 'd2' },
+                ],
+              }),
+              'application/test+x-www-form-urlencoded',
+            ),
+          ).toEqual({
+            contents: [
+              {
+                examples: [{ key: 'default', value: { k1: 'v1', k2: 'v2' } }],
+                mediaType: 'application/test+x-www-form-urlencoded',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    k1: { type: 'string', description: undefined },
+                    k2: { type: 'string', description: 'd2' },
+                  },
+                },
+              },
+            ],
+          });
+        });
+      });
+
+      describe('media type is not defined', () => {
+        it('returns body containing schema, example and application/x-www-form-urlencoded media type', () => {
+          expect(
+            transformBody(
+              new RequestBody({
+                mode: 'urlencoded',
+                urlencoded: [{ key: 'k1', value: 'v1' }],
+              }),
+            ),
+          ).toEqual({
+            contents: [
+              {
+                examples: [{ key: 'default', value: { k1: 'v1' } }],
+                mediaType: 'application/x-www-form-urlencoded',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    k1: { type: 'string', description: undefined },
+                  },
+                },
+              },
+            ],
+          });
+        });
+      });
+    });
+
+    describe('body is not defined', () => {
+      it('returns default empty body', () => {
+        expect(transformBody(new RequestBody({ mode: 'urlencoded' }))).toEqual({
+          contents: [
+            {
+              examples: [{ key: 'default', value: {} }],
+              mediaType: 'application/x-www-form-urlencoded',
+              schema: { properties: {}, type: 'object' },
+            },
+          ],
+        });
+      });
+
+      it('returns no body', () => {
+        expect(transformBody({ mode: 'urlencoded' } as RequestBody)).toBeUndefined();
+      });
+    });
+  });
+
+  describe('body is passed in unknown mode', () => {
+    it('returns no body', () => {
+      expect(transformBody({ mode: 'unknown' } as RequestBody)).toBeUndefined();
+    });
+  });
+});
---
+++ src/postman/transformers/params.ts
@@ -0,0 +1,143 @@
+import {
+  HttpParamStyles,
+  IHttpHeaderParam,
+  IHttpOperationRequestBody,
+  IHttpPathParam,
+  IHttpQueryParam,
+  IMediaTypeContent,
+} from '@stoplight/types';
+import { JSONSchema4 } from 'json-schema';
+import { FormParam, Header, PropertyList, QueryParam, RequestBody } from 'postman-collection';
+import * as toJsonSchema from 'to-json-schema';
+import * as typeIs from 'type-is';
+import { transformDescriptionDefinition, transformValueToSchema } from '../util';
+
+export function transformQueryParam(queryParam: QueryParam): IHttpQueryParam {
+  return {
+    name: queryParam.key || '',
+    style: HttpParamStyles.Form,
+    ...(queryParam.value ? transformValueToSchema(queryParam.value) : undefined),
+  };
+}
+
+export function transformHeader(header: Header): IHttpHeaderParam {
+  return {
+    name: header.key.toLowerCase(),
+    style: HttpParamStyles.Simple,
+    ...(header.value ? transformValueToSchema(header.value) : undefined),
+  };
+}
+
+export function transformPathParams(segments: string[]): IHttpPathParam[] {
+  return segments.reduce<IHttpHeaderParam[]>((params, segment) => {
+    if (segment.startsWith(':')) {
+      params.push({
+        name: segment.substring(1),
+        style: HttpParamStyles.Simple,
+      });
+    }
+
+    return params;
+  }, []);
+}
+
+export function transformBody(body: RequestBody, mediaType?: string): IHttpOperationRequestBody | undefined {
+  switch (body.mode) {
+    case 'raw':
+      if (!body.raw) return;
+      return { contents: [transformRawBody(body.raw, mediaType)] };
+
+    case 'formdata':
+      if (!body.formdata) return;
+      return {
+        contents: [transformParamsBody<FormParam>(body.formdata, mediaType || 'multipart/form-data')],
+      };
+
+    case 'urlencoded':
+      if (!body.urlencoded) return;
+      return {
+        contents: [transformParamsBody<QueryParam>(body.urlencoded, mediaType || 'application/x-www-form-urlencoded')],
+      };
+  }
+
+  return;
+}
+
+export function transformRawBody(raw?: string, mediaType: string = 'text/plain'): IMediaTypeContent {
+  if (raw && typeIs.is(mediaType, ['application/json', 'application/*+json'])) {
+    try {
+      const parsed = JSON.parse(raw);
+
+      return {
+        mediaType,
+        examples: [
+          {
+            key: 'default',
+            value: parsed,
+          },
+        ],
+        schema: toJsonSchema(parsed) as JSONSchema4,
+      };
+    } catch (e) {
+      /* noop, move on.. */
+    }
+  }
+
+  return {
+    mediaType,
+    examples:
+      raw === undefined // because '' is also false
+        ? undefined
+        : [
+            {
+              key: 'default',
+              value: raw,
+            },
+          ],
+  };
+}
+
+function transformParamsBody<T extends FormParam | QueryParam>(
+  params: PropertyList<T>,
+  mediaType: string,
+): IMediaTypeContent {
+  const paramsList: Array<{ name: string; schema: JSONSchema4; value: any }> = params.map(item => {
+    return {
+      name: item.key || generateId(),
+      schema: {
+        type: 'string',
+        description: item.description && transformDescriptionDefinition(item.description),
+      },
+      value: item.value,
+    };
+  }, undefined);
+
+  return {
+    mediaType,
+    schema: {
+      type: 'object',
+      properties: paramsList.reduce((props, param) => {
+        props[param.name] = param.schema;
+        return props;
+      }, {}),
+    },
+    examples: [
+      {
+        key: 'default',
+        value: paramsList.reduce((values, param) => {
+          values[param.name] = param.value;
+          return values;
+        }, {}),
+      },
+    ],
+  };
+}
+
+function generateId() {
+  return (
+    '_gen_' +
+    Math.round(Math.pow(8, 6) * Math.random())
+      .toString(16)
+      .padStart(6, '0')
+  );
+}
