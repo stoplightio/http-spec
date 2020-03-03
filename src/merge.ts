df9a74915657daf2f18e5cfa6c34d0d1eabe6da2
Karol Maciaszek <karol@maciaszek.pl>
2020-03-03 19:03:50 +0100

    test: added tests for merge operation

--- src/merge.ts
+++ src/merge.ts
@@ -1,13 +1,13 @@
-import { IHttpHeaderParam, IHttpOperation, IHttpOperationResponse, IMediaTypeContent } from '@stoplight/types';
+import { IHttpHeaderParam, IHttpOperation, IHttpOperationResponse, IMediaTypeContent, IServer } from '@stoplight/types';
 
 const mergeHeaders = mergeLists<IHttpHeaderParam>(
   (h1, h2) => h1.name === h2.name,
-  h1 => h1,
+  h1 => h1, // ignore header #2 if mediaTypes is equal
 );
 
 const mergeContents = mergeLists<IMediaTypeContent>(
   (c1, c2) => c1.mediaType === c2.mediaType,
-  c1 => c1,
+  c1 => c1, // ignore content #2 if mediaTypes is equal
 );
 
 export const mergeResponses = mergeLists<IHttpOperationResponse>(
@@ -19,11 +19,17 @@ export const mergeResponses = mergeLists<IHttpOperationResponse>(
   }),
 );
 
+const mergeServers = mergeLists<IServer>(
+  (s1, s2) => s1.url === s2.url,
+  s1 => s1, // ignore server #2 is url is equal
+);
+
 export const mergeOperations = mergeLists<IHttpOperation>(
   (o1, o2) => o1.path === o2.path && o1.method === o2.method,
   (o1, o2) => ({
     ...o1,
     responses: mergeResponses(o1.responses, o2.responses) as IHttpOperation['responses'],
+    servers: mergeServers(o1.servers || [], o2.servers || []),
   }),
 );
 
