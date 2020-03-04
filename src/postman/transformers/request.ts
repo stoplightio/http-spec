0ed79223531fcf7927a78c72090edc8aaef2e2c1
Karol Maciaszek <karol@maciaszek.pl>
2020-03-04 17:36:35 +0100

    fix: handles case when url.path is not defined

--- src/postman/transformers/request.ts
+++ src/postman/transformers/request.ts
@@ -15,7 +15,7 @@ export function transformRequest(request: Request): Required<IHttpOperationReque
   return {
     query: request.url.query.map(transformQueryParam),
     headers: request.headers.map(transformHeader),
-    path: transformPathParams(request.url.path),
+    path: transformPathParams(request.url.path || []),
     body: request.body ? transformBody(request.body, request.headers.get('content-type')?.toLowerCase()) : undefined,
   };
 }
