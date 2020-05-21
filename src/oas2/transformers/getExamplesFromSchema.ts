8971e2c484364c8b3672627e5b36ea1d749180e9
Jakub Jankowski <jakubj@stoplight.io>
2020-05-21 22:52:48 +0200

    fix: do not support oas2 schema x-example property

--- src/oas2/transformers/getExamplesFromSchema.ts
+++ src/oas2/transformers/getExamplesFromSchema.ts
@@ -2,15 +2,11 @@ import { isObject } from 'lodash';
 import { Schema } from 'swagger-schema-official';
 
 export function getExamplesFromSchema(data: Schema) {
-  return isObject(data)
-    ? 'x-examples' in data
-      ? isObject(data['x-examples'])
-        ? data['x-examples']
-        : void 0
-      : 'example' in data
-      ? { default: data.example }
-      : 'x-example' in data
-      ? { default: data['x-example'] }
+  return 'x-examples' in data
+    ? isObject(data['x-examples'])
+      ? data['x-examples']
       : void 0
+    : 'example' in data
+    ? { default: data.example }
     : void 0;
 }
