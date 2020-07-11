47c93a1f08fe16f6faffcdbb3d696b974262db06
lottamus <chrisnlott@gmail.com>
2020-07-10 20:17:07 -0500

    fix: handle invalid schema data

---
+++ src/oas2/transformers/__tests__/getExamplesFromSchema.test.ts
@@ -0,0 +1,26 @@
+import { getExamplesFromSchema } from "../getExamplesFromSchema";
+
+describe('getExamplesFromSchema', () => {
+  it('should ignore invalid data', () => {
+    // @ts-ignore
+    expect(getExamplesFromSchema(null)).toEqual({});
+  });
+
+  it('should work with x-examples', () => {
+    expect(getExamplesFromSchema({
+      'x-examples': {
+        'my-example': {}
+      }
+    })).toEqual({
+      'my-example': {}
+    });
+  });
+
+  it('should work with example', () => {
+    expect(getExamplesFromSchema({
+      'example': 'my-example'
+    })).toEqual({
+      default: 'my-example'
+    });
+  });
+});
