79902af6e646dfab29492d523d3efad4dfb72a05
Jakub Rożek <jakub@stoplight.io>
2020-09-08 00:20:14 +0200

    fix(oas): security flows transformation is incorrect

--- src/oas2/transformers/securities.ts
+++ src/oas2/transformers/securities.ts
@@ -7,7 +7,7 @@ import {
   IOauth2SecurityScheme,
   IOauthFlowObjects,
 } from '@stoplight/types';
-import { compact } from 'lodash';
+import { isString, pickBy } from 'lodash';
 import {
   ApiKeySecurity,
   OAuth2AccessCodeSecurity,
@@ -18,22 +18,22 @@ import {
   Spec,
 } from 'swagger-schema-official';
 
+import { SecurityWithKey } from '../../oas3/accessors';
+import { isDictionary } from '../../utils';
 import { getSecurities } from '../accessors';
+import { isSecurityScheme } from '../guards';
 
 /**
  * @param security the union with 'any' is purposeful. Passing strict types does not help much here,
  * because all these checks happen in runtime. I'm leaving 'Security' only for visibility.
  */
-function translateToFlows(
-  security: DeepPartial<
-    OAuth2AccessCodeSecurity | OAuth2ApplicationSecurity | OAuth2ImplicitSecurity | OAuth2PasswordSecurity
-  >,
-): IOauthFlowObjects {
+function translateToFlows(security: Dictionary<unknown>): IOauthFlowObjects {
   const flows: IOauthFlowObjects = {};
 
-  const scopes = (security.scopes as Dictionary<string>) || {};
-  const authorizationUrl = 'authorizationUrl' in security ? security.authorizationUrl || '' : '';
-  const tokenUrl = 'tokenUrl' in security ? security.tokenUrl || '' : '';
+  const scopes = isDictionary(security.scopes) ? pickBy<unknown, string>(security.scopes, isString) : {};
+  const authorizationUrl =
+    'authorizationUrl' in security && typeof security.authorizationUrl === 'string' ? security.authorizationUrl : '';
+  const tokenUrl = 'tokenUrl' in security && typeof security.tokenUrl === 'string' ? security.tokenUrl : '';
 
   if (security.flow === 'implicit') {
     flows.implicit = {
@@ -107,14 +107,16 @@ function translateToOauth2SecurityScheme(
   };
 }
 
-export function translateToSingleSecurity(security: DeepPartial<Security>, key: string) {
-  switch (security.type) {
-    case 'basic':
-      return translateToBasicSecurityScheme(security, key);
-    case 'apiKey':
-      return translateToApiKeySecurityScheme(security, key);
-    case 'oauth2':
-      return translateToOauth2SecurityScheme(security, key);
+export function translateToSingleSecurity(security: unknown, key: string) {
+  if (isSecurityScheme(security)) {
+    switch (security.type) {
+      case 'basic':
+        return translateToBasicSecurityScheme(security, key);
+      case 'apiKey':
+        return translateToApiKeySecurityScheme(security, key);
+      case 'oauth2':
+        return translateToOauth2SecurityScheme(security, key);
+    }
   }
 
   return;
@@ -125,5 +127,15 @@ export function translateToSecurities(
   operationSecurity: Dictionary<string[], string>[] | undefined,
 ): HttpSecurityScheme[][] {
   const securities = getSecurities(document, operationSecurity);
-  return securities.map(security => compact(security.map(sec => translateToSingleSecurity(sec, sec.key))));
+
+  return securities.map(security =>
+    security.reduce<SecurityWithKey[]>((transformedSecurities, sec) => {
+      const transformed = translateToSingleSecurity(sec, sec.key);
+      if (transformed) {
+        transformedSecurities.push(transformed);
+      }
+
+      return transformedSecurities;
+    }, []),
+  );
 }
