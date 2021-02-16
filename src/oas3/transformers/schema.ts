d04c1622f7da0ccb55016905afc674a9cb1c16f8
Jakub Rożek <jakub@stoplight.io>
2021-02-16 19:05:38 +0200

    chore: document changes

--- src/oas3/transformers/schema.ts
+++ src/oas3/transformers/schema.ts
@@ -3,6 +3,23 @@ import { isObject } from 'lodash';
 import type { SchemaObject } from 'openapi3-ts/src/model/OpenApi';
 
 // based on https://github.com/openapi-contrib/openapi-schema-to-json-schema
+// and specifically on https://github.com/openapi-contrib/openapi-schema-to-json-schema/blob/master/lib/converters/schema.js
+// the above converter is the only one we actually used, while https://github.com/openapi-contrib/openapi-schema-to-json-schema/blob/master/lib/converters/parameter.js was never utilized
+/**
+ * Differences
+ * * Removed options:
+ * * * strictMode (this was actually introduced for Stoplight, it's enabled by default now)
+ * * * dateToDateTime (was unused)
+ * * * cloneSchema (true by default now, we never want to alter schema)
+ * * * supportPatternProperties & patternPropertiesHandler (were unused)
+ * * * removeReadOnly & removeWriteOnly (were unused)
+ * * * removeProps (was unused)
+ *
+ * * Changed behavior:
+ * * * keepNotSupported (whitelist) -> pruneNotSupported (blacklist)
+ * * * empty properties are not removed
+ */
+
 const settings = {
   MIN_INT_32: 0 - Math.pow(2, 31),
   MAX_INT_32: Math.pow(2, 31) - 1,
