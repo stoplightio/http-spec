5334b25f6d583516dacc06f05814bf1bc4257d9f
Jakub Rożek <jakub@stoplight.io>
2022-03-30 12:40:52 +0200

    fix: improve entry points

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './oas';
 export * from './oas2';
 export * from './oas3';
 export * from './postman';
+export * from './types';
--- src/oas/index.ts
+++ src/oas/index.ts
@@ -1,2 +1,3 @@
 export * from './operation';
+export { convertSchema as convertToJsonSchema } from './transformers/index';
 export * from './types';
