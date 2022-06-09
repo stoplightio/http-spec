9bb8aed7b85524eb1426464de87136cf5f769b79
Jakub Rożek <jakub@stoplight.io>
2022-06-09 03:04:15 +0200

    chore: export bundle types

--- src/oas/index.ts
+++ src/oas/index.ts
@@ -3,10 +3,12 @@ export { resolveRef as defaultRefResolver } from './resolver';
 export { convertSchema as convertToJsonSchema } from './transformers/index';
 export type {
   Oas2HttpOperationTransformer,
+  Oas2HttpServiceBundle,
   Oas2HttpServiceTransformer,
   Oas2TransformOperationOpts,
   Oas2TransformServiceOpts,
   Oas3HttpOperationTransformer,
+  Oas3HttpServiceBundle,
   Oas3HttpServiceTransformer,
   Oas3TransformOperationOpts,
   Oas3TransformServiceOpts,
