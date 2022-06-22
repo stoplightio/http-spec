0224ef1d9a29e405ae60efdce817aab9a4f9207e
Jakub Rożek <jakub@stoplight.io>
2022-06-22 20:11:06 +0200

    chore: expose generators

--- src/generators.ts
+++ src/generators.ts
@@ -1,6 +1,6 @@
 type Context = { parentId: string };
 
-function hash(dependencies: (string | undefined)[]) {
+function join(dependencies: (string | undefined)[]) {
   return dependencies.join('-');
 }
 
@@ -14,47 +14,47 @@ function sanitizePath(path: string) {
 
 export const idGenerators = {
   tag: (props: Context & { name: string }) => {
-    return hash(['tag', props.name]);
+    return join(['tag', props.name]);
   },
 
   schema: (props: Context & { key: string }) => {
-    return hash(['schema', props.parentId, props.key]);
+    return join(['schema', props.parentId, props.key]);
   },
 
   schemaProperty: (props: Context & { key: string | number }) => {
-    return hash(['schema_property', props.parentId, String(props.key)]);
+    return join(['schema_property', props.parentId, String(props.key)]);
   },
 
   example: (props: Context & { nameOrKey: string }) => {
-    return hash(['example', props.parentId, props.nameOrKey]);
+    return join(['example', props.parentId, props.nameOrKey]);
   },
 
   httpPath: (props: Context & { path: string }) => {
-    return hash(['http_path', props.parentId, sanitizePath(props.path)]);
+    return join(['http_path', props.parentId, sanitizePath(props.path)]);
   },
 
   httpOperation: (props: Context & { method: string; path: string }) => {
-    return hash(['http_operation', props.parentId, props.method, sanitizePath(props.path)]);
+    return join(['http_operation', props.parentId, props.method, sanitizePath(props.path)]);
   },
 
   httpPathParam: (props: Context & { nameOrKey: string }) => {
-    return hash(['http_path_param', props.parentId, props.nameOrKey]);
+    return join(['http_path_param', props.parentId, props.nameOrKey]);
   },
 
   httpQuery: (props: Context & { nameOrKey: string }) => {
-    return hash(['http_query', props.parentId, props.nameOrKey]);
+    return join(['http_query', props.parentId, props.nameOrKey]);
   },
 
   httpCookie: (props: Context & { nameOrKey: string }) => {
-    return hash(['http_cookie', props.parentId, props.nameOrKey]);
+    return join(['http_cookie', props.parentId, props.nameOrKey]);
   },
 
   httpHeader: (props: Context & { nameOrKey: string }) => {
-    return hash(['http_header', props.parentId, props.nameOrKey]);
+    return join(['http_header', props.parentId, props.nameOrKey]);
   },
 
   httpRequestBody: (props: { parentId: string; consumes?: string[] }) => {
-    return hash([
+    return join([
       'http_request_body',
       props.parentId,
       ...(Array.isArray(props.consumes) ? sortAlphabetically(props.consumes) : []),
@@ -62,22 +62,22 @@ export const idGenerators = {
   },
 
   httpMedia: (props: { parentId: string; mediaType?: string }) => {
-    return hash(['http_media', props.parentId, props.mediaType]);
+    return join(['http_media', props.parentId, props.mediaType]);
   },
 
   httpSecurity: (props: { parentId: string; nameOrKey?: string }) => {
-    return hash(['http_security', props.parentId, props.nameOrKey]);
+    return join(['http_security', props.parentId, props.nameOrKey]);
   },
 
   httpServer: (props: { parentId: string; url: string }) => {
-    return hash(['http_server', props.parentId, props.url]);
+    return join(['http_server', props.parentId, props.url]);
   },
 
   /**
    * Supply response code if available, key otherwise (e.g. for shared responses)
    */
   httpResponse: (props: { parentId: string; codeOrKey: string; produces?: string[] }) => {
-    return hash([
+    return join([
       'http_response',
       props.parentId,
       props.codeOrKey,
