7d4dbe0de3b6333a6c063be37a54a292ea234049
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2022-07-26 14:58:43 -0500

    temp do not hash to make debugging easier

--- src/hash.ts
+++ src/hash.ts
@@ -3,4 +3,5 @@ import * as fnv from 'fnv-plus';
 
 import type { IdGenerator } from './types';
 
-export const hash: IdGenerator = input => fnv.fast1a52hex(input);
+// export const hash: IdGenerator = input => fnv.fast1a52hex(input);
+export const hash: IdGenerator = input => input;
