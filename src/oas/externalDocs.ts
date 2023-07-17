281f749645716b5ea4c7bfba0fb72c025c67358d
Ed Vinyard <ed@stoplight.io>
2023-07-17 17:14:53 -0500

    fix: 15432 unit tests for toExternalDocs()

--- src/oas/externalDocs.ts
+++ src/oas/externalDocs.ts
@@ -1,11 +1,50 @@
 import { IExternalDocs } from '@stoplight/types';
 
-export function toExternalDocs(externalDocs: any): { externalDocs?: IExternalDocs } | undefined {
-  if (!externalDocs) return undefined;
-  if (typeof externalDocs !== 'object') return undefined;
-  if (!externalDocs?.url) return undefined;
+/**
+ * Extract an OpanAPI `externalDocs` object
+ * @return an object that includes an `externalDocs` object with all well-formed
+ * properties from the supplied object, or an empty object
+ */
+export function toExternalDocs(externalDocs: any): { externalDocs?: IExternalDocs } {
+  if (!externalDocs || typeof externalDocs !== 'object') {
+    return {};
+  }
 
-  const result: IExternalDocs = { url: externalDocs.url };
-  if (externalDocs.description) result['description'] = externalDocs.description;
-  return { externalDocs: result };
+  const url = getUrl(externalDocs);
+  if (url === undefined) {
+    return {};
+  }
+
+  return {
+    externalDocs: {
+      url,
+      description: getDescription(externalDocs),
+    },
+  };
+}
+
+const ALL_WHITESPACE: RegExp = /^\s*$/;
+
+/**
+ * @returns the `url` property, or undefined if the object lacks it
+ */
+function getUrl(externalDocs: { url?: any } | undefined): string | undefined {
+  const url = externalDocs?.url;
+  if (!url || typeof url !== 'string' || ALL_WHITESPACE.test(url)) {
+    return undefined;
+  }
+
+  return url;
+}
+
+/**
+ * @returns the `description` property, or undefined if the object lacks it
+ */
+function getDescription(externalDocs: { description?: any }): string | undefined {
+  const description = externalDocs.description;
+  if (!description || typeof description !== 'string' || ALL_WHITESPACE.test(description)) {
+    return undefined;
+  }
+
+  return description;
 }
