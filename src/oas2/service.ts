5d1f082e9d7608cd769513849bfb8b1edea70560
chohmann <chelsea.hohmann@gmail.com>
2023-07-24 16:00:26 -0500

    cleanup

--- src/oas2/service.ts
+++ src/oas2/service.ts
@@ -11,7 +11,6 @@ import { transformOasService } from '../oas/service';
 import { translateToComponents } from '../oas/transformers/components';
 import { translateSchemaObjectFromPair } from '../oas/transformers/schema';
 import { Oas2HttpServiceBundle, Oas2HttpServiceTransformer, OasVersion } from '../oas/types';
-// import { translateToResponse } from '../oas3/transformers/responses';
 import { ArrayCallbackParameters, RefResolver } from '../types';
 import { entries } from '../utils';
 import { transformOas2Operations } from './operation';
