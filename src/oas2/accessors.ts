9762cd2ffc97dc26d5b93d8fc5ee482eeef84520
Derek Thompson <dotslashderek@gmail.com>
2023-10-03 08:24:07 -0400

    fix(oas2): operation optional security support (#254)

--- src/oas2/accessors.ts
+++ src/oas2/accessors.ts
@@ -3,18 +3,24 @@ import type { DeepPartial } from '@stoplight/types';
 import type { Operation, Security, Spec } from 'swagger-schema-official';
 import pickBy = require('lodash.pickby');
 
+import { isEmpty } from 'lodash';
+
 import { isNonNullable, isString } from '../guards';
 import { isSecurityScheme } from './guards';
 
 export type SecurityWithKey = Security & { key: string };
 
 export function getSecurities(spec: DeepPartial<Spec>, operationSecurity: unknown): SecurityWithKey[][] {
-  const globalSecurities = getSecurity(spec.security, spec.securityDefinitions || {});
-  const operationSecurities = getSecurity(operationSecurity, spec.securityDefinitions || {});
+  if (!isNonNullable(spec.securityDefinitions) || isEmpty(spec.securityDefinitions)) return [];
 
-  const securities = !!operationSecurity ? operationSecurities : globalSecurities;
+  const security = !!operationSecurity ? operationSecurity : spec.security;
+  const securities = getSecurity(security, spec.securityDefinitions);
 
-  return securities.filter(a => a.length);
+  if (isOptionalSecurity(security)) {
+    securities.push([]);
+  }
+
+  return securities;
 }
 
 export function getProduces(spec: DeepPartial<Spec>, operation: DeepPartial<Operation>) {
@@ -30,29 +36,31 @@ function getSecurity(security: unknown, definitions: DeepPartial<Spec['securityD
     return [];
   }
 
-  return security.map(sec => {
-    if (!isPlainObject(sec)) return [];
-    return Object.keys(sec)
-      .map(key => {
-        const def = definitions[key];
+  return security
+    .map(sec => {
+      if (!isPlainObject(sec)) return [];
+      return Object.keys(sec)
+        .map(key => {
+          const def = definitions[key];
 
-        if (isSecurityScheme(def)) {
-          const defCopy = { ...def, key };
-          const secKey = sec[key];
-          const scopes = Array.isArray(secKey) ? secKey : [];
+          if (isSecurityScheme(def)) {
+            const defCopy = { ...def, key };
+            const secKey = sec[key];
+            const scopes = Array.isArray(secKey) ? secKey : [];
 
-          // Filter definition scopes by operation scopes
-          if (defCopy.type === 'oauth2' && scopes.length) {
-            defCopy.scopes = pickBy(defCopy.scopes, (_val, s: string) => scopes.includes(s));
+            // Filter definition scopes by operation scopes
+            if (defCopy.type === 'oauth2' && scopes.length) {
+              defCopy.scopes = pickBy(defCopy.scopes, (_val, s: string) => scopes.includes(s));
+            }
+
+            return defCopy;
           }
 
-          return defCopy;
-        }
-
-        return null;
-      })
-      .filter(isNonNullable);
-  });
+          return null;
+        })
+        .filter(isNonNullable);
+    })
+    .filter(scheme => !isEmpty(scheme));
 }
 
 export function normalizeProducesOrConsumes(input: unknown): string[] {
@@ -78,4 +86,8 @@ export function getExamplesFromSchema(data: unknown): Record<string, unknown> {
     ...(isPlainObject(data['x-examples']) && { ...data['x-examples'] }),
     ...('example' in data && { default: data.example }),
   };
+}
+
+function isOptionalSecurity(security: unknown): boolean {
+  return Array.isArray(security) && security.length > 1 && security.some(scheme => isEmpty(scheme));
 }
